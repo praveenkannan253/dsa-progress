a67e32b7c39ef20015bba741aa362249e6a443b2
praveenkannan253 <praveenkannan253@gmail.com>
2026-01-01 21:46:04 +0530

    DFS Representation

---
+++ Graphs/dfs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class dfs{
+    static void DFS(int node,ArrayList<ArrayList<Integer>>adj,int n,boolean[] vis){
+        vis[node]=true;
+        System.out.print(node+ " ");
+        for(Integer neighbor:adj.get(node)){
+            if(!vis[neighbor]){
+                DFS(neighbor,adj,n,vis);
+            }
+        }
+    }
+    public static void main(String[] args){
+        int n=7;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(1).add(4);
+        adj.get(2).add(5);
+        adj.get(2).add(6);        
+        boolean[] vis=new boolean[n];
+        DFS(0,adj,n,vis);
+    }
+}
